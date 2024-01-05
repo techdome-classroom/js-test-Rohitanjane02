b65e5d542b96ee90866cc8f13f7ef24a57a7942f
Rohit Anjane <rohitanjane999@gmail.com>
2024-01-05 13:34:24 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,22 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+      const ch = s[i];
+  
+      if (ch === '(' || ch === '[' || ch === '{') {
+        stack.push(char);
+      } else if (ch === ')' || ch === ']' || ch === '}') {
+       
+        const lastOpen = stack.pop();
+  
+        if ( (ch === ')' && lastOpen !== '(') || (ch === ']' && lastOpen !== '[') ||  (ch === '}' && lastOpen !== '{')) {
+          return false;
+        }
+      }
+    }
 };
 
 module.exports = { isValid };
